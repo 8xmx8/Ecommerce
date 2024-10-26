62d15ccc5c0e8549ec4a5ce5615c909bd4bb9134
8xmx8 <2647369456@qq.com>
2024-10-26 20:29:25 +0800

    project for docker deploy

--- manifest/docker/docker.sh
+++ manifest/docker/docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This shell is executed before docker build.
-
+# github actions to add env_values for dockerhub password, server on dockerhub is opt Directory.
+docker run -v "$(pwd)":/opt/recognitionsystem golangci/golangci-lint:v1.41 sh /opt/recognitionsystem/build/ci/static_check/run_golangci_lint.sh
 
 
 
